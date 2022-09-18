cfe474180da29aafd4eca84c78c9ed3ae36276b4
RealSoerensen <patricklykke@live.dk>
2022-09-18 14:28:57 +0200

    Updated comments and file-structure

--- app/src/main/java/UCNDiscordBot/Listeners/ReactionListener/ReactionListener.java
+++ app/src/main/java/UCNDiscordBot/Listeners/ReactionListener/ReactionListener.java
@@ -7,33 +7,35 @@ import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 // Class to give rank on react
-public class listenReact extends ListenerAdapter {
+public class ReactionListener extends ListenerAdapter {
     @Override
     public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
-        //This runs if anyone reacts to a message
-        
-        //The eventhandler
+        // This runs if anyone reacts to a message
+
+        // The eventhandler
         if (event.getUser().isBot())
-            //checks if the owner of the message is a bot
+            // checks if the owner of the message is a bot
             return;
         if (isChannel(event, "roles")) {
-            //This is true if the reaction happened in the channel called role
+            // This is true if the reaction happened in the channel called role
 
-            //PATRICK! could you please describe this
+            // Assign role to user
             assignRole(event);
         }
     }
 
-    private void assignRole(MessageReactionAddEvent event){
-        //PATRICK! could you please describe this
+    private void assignRole(MessageReactionAddEvent event) {
+        // Check if the reaction is the checkmark
         if (event.getReaction().getEmoji().asUnicode().equals(Emoji.fromUnicode("U+2705"))) {
+            // Get message id
             String messageID = event.getMessageId();
+            // Get message text using message id
             String message = event.getChannel().retrieveMessageById(messageID).complete()
                     .getContentDisplay();
+            // Loop over all roles
             for (int i = 1; i < event.getGuild().getRoles().size(); i++) {
                 // Get role id
                 String roleID = event.getGuild().getRoles().get(i).getId();
-                
 
                 // Get the role name
                 String roleName = event.getGuild().getRoles().get(i).getName();
@@ -44,12 +46,13 @@ public class listenReact extends ListenerAdapter {
                 }
             }
         }
+        // Reset reactions
         resetReactions(event);
     }
 
-    private boolean isChannel(MessageReactionAddEvent event, String channel){
-        //Checks if the message was written in a specific channel
-        //this will be used is the eventhandler
+    private boolean isChannel(MessageReactionAddEvent event, String channel) {
+        // Checks if the message was written in a specific channel
+        // this will be used is the eventhandler
         return event.getChannel().getName().equals(channel);
     }
 
@@ -62,7 +65,6 @@ public class listenReact extends ListenerAdapter {
         try {
             Thread.sleep(50);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         event.getGuildChannel().addReactionById(event.getMessageId(), Emoji.fromUnicode("U+2705")).queue();
